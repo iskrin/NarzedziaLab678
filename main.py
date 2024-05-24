e0b5d99b8ebdd9d654ea36a93b02a14d337bfd5f
Wiktor <dsw51748@student.dsw.edu.pl>
2024-05-25 01:03:33 +0200

    dodano ładowanie formatu json do obiektu

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import argparse
+import json
 
 def pharseArguments():
-    parser = argparse.ArgumentParser(description="Program do kowersji między .xml, .yaml, .json") 
-    parser.add_argument('currentFormat', type=str, help='Plik który chcemy przekonwertować z obecnym rozszerzeniem')
-    #parser.add_argument('targetFormat', type=str, help='Plik który chcemy przekonwertować z docelowym rozszerzeniem')
+    parser = argparse.ArgumentParser(description="Converter between .xml, .yaml, .json") 
+    parser.add_argument('sourceFile', type=str, help='Source file that we want to convert')
+    #parser.add_argument('targetFile', type=str, help='Target file with a desired extension')
     args = parser.parse_args()  
     return args
 
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        return data
+    except ValueError:
+        print("Error while reading source file")
+
+args = pharseArguments()
+
+sourceFile = args.sourceFile
+currentFormat = sourceFile.split(".")[-1]
+
+output = read_json(sourceFile)
+print(output)
